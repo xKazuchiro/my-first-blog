0dab9fb23425df8cd3dd3154870c8703064a9241
Dominik <dominik.labudda98@gmail.com>
2017-03-22 11:11:10 +0100

    Add styles and views for once entry on a page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
+# def post_new(request):
+#     form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+#
+#
+# if request.method == "POST":
+#     form = PostForm(request.POST)
+# else:
+#     form = PostForm()
+#
+# if form.is_valid():
+#     post = form.save(commit=False)
+#     post.author = request.user
+#     post.published_date = timezone.now()
+#     post.save()
